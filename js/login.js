825700a1d0074a1c587c52a7d60fb5046b55cc71
Jahid Hasan <jahidhasanmisir@gmail.com>
2024-09-27 18:02:49 +0600

    login page design created

---
+++ js/login.js
@@ -0,0 +1,10 @@
+// console.log('login button added');
+
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    console.log('login button is clicked');
+
+    const phoneNumber = document.getElementById('phone-number');
+    console.log
+})
